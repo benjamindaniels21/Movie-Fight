73e054100d0e61375e2ca0993ee01edf90140b0b
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-02 09:32:53 -0500

    fetching from API

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "9649bc20",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value);
+});
