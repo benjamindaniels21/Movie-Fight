39cad463c74a4e3026e0f9c9c632a46683daf1df
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-03 13:03:59 -0500

    altering the DOM add divs

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const onInput = (e) => {
-  fetchData(e.target.value);
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <h1>${movie.Title}</h1>
+      <img src="${movie.Poster}"/>
+    `;
+    document.querySelector("#target").appendChild(div);
+  }
+
+  console.log(movies);
 };
 
 input.addEventListener("input", debounce(onInput, 500));
